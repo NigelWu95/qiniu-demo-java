0556964ae069acb18f8ea4e1e1674f58b865f54c
wubingheng <wubinghengajw@outlook.com>
2018-09-12 03:18:15 +0800

    1、七牛文件 etag 和图片 url fop 处理 etag 计算。 2、字符转换处理工具类。

---
+++ src/main/java/com/qiniu/examples/util/QiniuEtagUtil.java
@@ -0,0 +1,134 @@
+package com.qiniu.examples.util;
+
+import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import javax.xml.bind.DatatypeConverter;
+
+public class QiniuEtagUtil {
+
+    public static void main(String[] args) {
+
+        try {
+            String srcEtag = "FrXIWGT7eB94QR46knD4TbH5TGdc";
+//            srcEtag = calcETag("");
+            String tarEtag = "AN751gvBIGo6x37EJDcZ8dVRLb_7";
+            String fopEtag = calcFopEtag(srcEtag, "imageMogr2/rotate/90/crop/!2105x1440a227a0/thumbnail/1866x1276");
+
+            System.out.println("source etag: " + srcEtag);
+            System.out.println("target etag: " + tarEtag);
+            System.out.println("fop etag is " + fopEtag + ", and it is " + (tarEtag.equals(fopEtag) ? "" : "not ") + "equal with tarEtag");
+
+            System.out.println("fop etag hex string is: " + CharactersUtil.bytesToHexString(checkHashFromEtag(fopEtag)));
+            System.out.println("add preamble to origin hash: " + CharactersUtil.bytesToHexString(addPreambleToHash(
+                    CharactersUtil.decode("def9d60bc1206a3ac77ec4243719f1d5512dbffb".toCharArray()), (byte)0x16)));
+            System.out.println("add preamble to origin hash: " + CharactersUtil.bytesToHexString(movePreambleFromEtag(tarEtag)));
+
+        } catch (NoSuchAlgorithmException ex) {
+            System.err.println("Unsupported algorithm: " + ex.getMessage());
+        }
+    }
+
+    private static final int CHUNK_SIZE = 1 << 22;
+
+    public static byte[] sha1(byte[] data) throws NoSuchAlgorithmException {
+        MessageDigest mDigest = MessageDigest.getInstance("sha1");
+        return mDigest.digest(data);
+    }
+
+    public static String urlSafeBase64Encode(byte[] data) {
+        String encodedString = DatatypeConverter.printBase64Binary(data);
+        encodedString = encodedString.replace('+', '-').replace('/', '_');
+        return encodedString;
+    }
+
+    public static String calcETag(String fileName) throws IOException, NoSuchAlgorithmException {
+        String etag = "";
+        File file = new File(fileName);
+        if (!(file.exists() && file.isFile() && file.canRead())) {
+            System.err.println("Error: File not found or not readable");
+            return etag;
+        }
+        long fileLength = file.length();
+        FileInputStream inputStream = new FileInputStream(file);
+        if (fileLength <= CHUNK_SIZE) {
+            byte[] fileData = new byte[(int) fileLength];
+            inputStream.read(fileData, 0, (int) fileLength);
+            byte[] sha1Data = sha1(fileData);
+            int sha1DataLen = sha1Data.length;
+            byte[] hashData = new byte[sha1DataLen + 1];
+            System.arraycopy(sha1Data, 0, hashData, 1, sha1DataLen);
+            hashData[0] = 0x16;
+            etag = urlSafeBase64Encode(hashData);
+        } else {
+            int chunkCount = (int) (fileLength / CHUNK_SIZE);
+            if (fileLength % CHUNK_SIZE != 0) {
+                chunkCount += 1;
+            }
+            byte[] allSha1Data = new byte[0];
+            for (int i = 0; i < chunkCount; i++) {
+                byte[] chunkData = new byte[CHUNK_SIZE];
+                int bytesReadLen = inputStream.read(chunkData, 0, CHUNK_SIZE);
+                byte[] bytesRead = new byte[bytesReadLen];
+                System.arraycopy(chunkData, 0, bytesRead, 0, bytesReadLen);
+                byte[] chunkDataSha1 = sha1(bytesRead);
+                byte[] newAllSha1Data = new byte[chunkDataSha1.length + allSha1Data.length];
+                System.arraycopy(allSha1Data, 0, newAllSha1Data, 0, allSha1Data.length);
+                System.arraycopy(chunkDataSha1, 0, newAllSha1Data, allSha1Data.length, chunkDataSha1.length);
+                allSha1Data = newAllSha1Data;
+            }
+            byte[] allSha1DataSha1 = sha1(allSha1Data);
+            byte[] hashData = new byte[allSha1DataSha1.length + 1];
+            System.arraycopy(allSha1DataSha1, 0, hashData, 1, allSha1DataSha1.length);
+            hashData[0] = (byte) 0x96;
+            etag = urlSafeBase64Encode(hashData);
+        }
+        inputStream.close();
+        return etag;
+    }
+
+    public static byte[] checkHashFromEtag(String etag) {
+
+        String encodedString = etag.replace('-', '+').replace('_', '/');
+        byte[] encodedStringHash = DatatypeConverter.parseBase64Binary(encodedString);
+        if (etag.equals(urlSafeBase64Encode(encodedStringHash))) {
+            return encodedStringHash;
+        } else {
+            return DatatypeConverter.parseBase64Binary(etag);
+        }
+    }
+
+    public static byte[] addPreambleToHash(byte[] hash, byte preamble) {
+        byte[] hashData = new byte[hash.length + 1];
+        System.arraycopy(hash, 0, hashData, 1, hash.length);
+        hashData[0] = preamble;
+        return hashData;
+    }
+
+    public static byte[] movePreambleFromEtag(String etag) {
+        String encodedString = etag.replace('-', '+').replace('_', '/');
+        byte[] srcEtagHashBytes = Base64.getDecoder().decode(encodedString);
+        byte[] hashBytes = new byte[srcEtagHashBytes.length - 1];
+        System.arraycopy(srcEtagHashBytes, 1, hashBytes, 0, srcEtagHashBytes.length - 1);
+        return hashBytes;
+    }
+
+    public static String calcFopEtag(String srcEtag, String processCommand) throws NoSuchAlgorithmException {
+
+        byte[] srcEtagHashBytes = Base64.getDecoder().decode(srcEtag);
+        byte[] fopBytes = new byte[]{0x01, 0x01};
+        byte[] fopParamsBytes = ( "cmd=" + processCommand + "&sp=").getBytes();
+        byte[] hashData = new byte[srcEtagHashBytes.length + fopBytes.length + fopParamsBytes.length];
+        System.arraycopy(srcEtagHashBytes, 0, hashData, 0, srcEtagHashBytes.length);
+        System.arraycopy(fopBytes, 0, hashData, srcEtagHashBytes.length, fopBytes.length);
+        System.arraycopy(fopParamsBytes, 0, hashData, srcEtagHashBytes.length + fopBytes.length, fopParamsBytes.length);
+        byte[] allSha1DataSha1 = sha1(hashData);
+        byte[] finalHashData = new byte[allSha1DataSha1.length + 1];
+        System.arraycopy(allSha1DataSha1, 0, finalHashData, 1, allSha1DataSha1.length);
+        finalHashData[0] = (byte) 0x00;
+        String etag = urlSafeBase64Encode(finalHashData);
+
+        return etag;
+    }
+}
