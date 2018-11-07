0a6decbc50bef223199a9749e8d2e17eb7c9a7f0
wubingheng <wubinghengajw@outlook.com>
2018-11-08 07:22:51 +0800

    oss source domain publish.

---
+++ src/main/java/com/qiniu/examples/oss/KodoDomainPublish.java
@@ -0,0 +1,34 @@
+package com.qiniu.examples.oss;
+
+import com.qiniu.common.Config;
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Client;
+import com.qiniu.util.*;
+
+public class KodoDomainPublish {
+
+    public static void main(String[] args) {
+
+        Config config = Config.getInstance();
+        String accessKey = config.getAccesskey();
+        String secretKey = config.getSecretKey();
+
+        Auth auth = Auth.create(accessKey, secretKey);
+        String tbl = "";
+        String url = "http://api.qiniu.com//publish/" + UrlSafeBase64.encodeToString("image.diyidan.net")
+                + "/from/" + tbl + "/domaintype/1";
+        StringMap headers = auth.authorization(url, null, "application/json");
+
+
+        Client client = new Client();
+        com.qiniu.http.Response response = null;
+        try {
+            response = client.post(url, "", headers);
+            System.out.println(response.bodyString());
+        } catch (QiniuException e) {
+            e.printStackTrace();
+        }
+
+        response.close();
+    }
+}
