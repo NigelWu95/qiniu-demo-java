08d8d32297ded46edaa36128eeed06de5ab40b25
wubingheng <wubinghengajw@outlook.com>
2018-11-12 19:57:20 +0800

    update.

--- src/main/java/com/qiniu/util/EncryptUtil.java
+++ src/main/java/com/qiniu/util/EncryptUtil.java
@@ -5,7 +5,7 @@ import org.apache.commons.codec.digest.DigestUtils;
 public class EncryptUtil {
 
     public static void main(String[] args) {
-        String domain = "image.diyidan.net";
+        String domain = "xxx.xxx.com";
         System.out.println(DigestUtils.shaHex(domain));
     }
 }
