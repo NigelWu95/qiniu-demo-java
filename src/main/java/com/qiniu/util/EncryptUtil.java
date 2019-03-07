30453e865215543f49764dd37fac4a5883b2eeff
wubingheng <wubinghengajw@outlook.com>
2019-03-07 11:49:22 +0800

    update.

--- src/main/java/com/qiniu/util/EncryptUtil.java
+++ src/main/java/com/qiniu/util/EncryptUtil.java
@@ -9,14 +9,6 @@ public class EncryptUtil {
 
     public static void main(String[] args) {
         List<String> domains = new ArrayList<String>(){{
-            add("xiangha-dsf1-rsa.cdn.xiangha.com");
-            add("xiangha-f1-rsa.cdn.xiangha.com");
-            add("xiangha-forcoo-rsa.cdn.xiangha.com");
-            add("xiangha-img-rsa.cdn.xiangha.com");
-            add("xiangha-m3u8video-rsa.cdn.xiangha.com");
-            add("xiangha-static-rsa.cdn.xiangha.com");
-            add("xiangha-temp-rsa.cdn.xiangha.com");
-            add("xiangha-video-rsa.cdn.xiangha.com");
         }};
 //        String domain = "xxx.xxx.com";
         for (String domain : domains) {
